0cfca75cf1d7d28fba3e51a304c138bd8334383c
vaheeD <khoshnud@gmail.com>
2026-06-14 23:58:39 +0330

    fix: update SSL request progress handling to support both object and array response formats

--- ci/phase0_repro.sh
+++ ci/phase0_repro.sh
@@ -126,7 +126,7 @@ record_step PASS "multiple-proxied-records" "two proxied records are stored and 
 
 api_post "${CORE_URL}/api/v1/domains/${DOMAIN_ID}/ssl/request" "{}"
 assert_http_status "$HTTP_CODE" "200" "SSL request endpoint should accept request"
-if ! jq -e '(.data.status and (.data.progress != null or .data.job_id != null or .data.certificate_id != null)) or ((.data | type) == "array" and (.data[0].status != null))' <<<"$HTTP_BODY" >/dev/null; then
+if ! jq -e 'if (.data | type) == "array" then (.data[0].status != null) else (.data.status and (.data.progress != null or .data.job_id != null or .data.certificate_id != null)) end' <<<"$HTTP_BODY" >/dev/null; then
   phase0_expect_failure "ssl-request-progress" "SSL request should return user-visible progress/job/certificate status fields"
 fi
 record_step PASS "ssl-request-progress" "SSL request returned visible progress metadata"
